0d3f2f5f1c3b2bf157bf8b54cbee2e5877b2003c
raviteja <raviteja.karri74@gmail.com>
2018-08-17 17:00:59 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,78 +1,91 @@
-package com.development.logging_test;
+package com.development.love_story;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.development.love_story.fragment.MyPostsFragment;
+import com.development.love_story.fragment.MyTopPostsFragment;
+import com.development.love_story.fragment.RecentPostsFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends BaseActivity {
 
-    public static final int INDEX_SIMPLE_LOGIN = 0;
-    public static final int INDEX_CUSTOM_LOGIN = 1;
+    private static final String TAG = "MainActivity";
 
-    private static final String STATE_SELECTED_FRAGMENT_INDEX = "selected_fragment_index";
-    public static final String FRAGMENT_TAG = "fragment_tag";
-    private FragmentManager mFragmentManager;
+    private FragmentPagerAdapter mPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFragmentManager = getSupportFragmentManager();
 
-    }
+        // Create the adapter that will return a fragment for each section
+        mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            private final Fragment[] mFragments = new Fragment[] {
+                    new RecentPostsFragment(),
+                    new MyPostsFragment(),
+                    new MyTopPostsFragment(),
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+            };
+            private final String[] mFragmentNames = new String[] {
+                    getString(R.string.heading_recent),
+                    getString(R.string.heading_my_posts),
+                    getString(R.string.heading_my_top_posts)
+            };
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments[position];
+            }
+            @Override
+            public int getCount() {
+                return mFragments.length;
+            }
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mFragmentNames[position];
+            }
+        };
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = findViewById(R.id.container);
+        mViewPager.setAdapter(mPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        // Button launches NewPostActivity
+        findViewById(R.id.fab_new_post).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewPostActivity.class));
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        int i = item.getItemId();
+        if (i == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
             return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
-
-        if (id == R.id.action_simple_login) {
-            toggleFragment(INDEX_SIMPLE_LOGIN);
-            return true;
-        }
-        if (id == R.id.action_custom_login) {
-            toggleFragment(INDEX_CUSTOM_LOGIN);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
-    private void toggleFragment(int index) {
-        Fragment fragment = mFragmentManager.findFragmentByTag(FRAGMENT_TAG);
-        FragmentTransaction transaction = mFragmentManager.beginTransaction();
-        switch (index){
-            case INDEX_SIMPLE_LOGIN:
-                transaction.replace(android.R.id.content, new FragmentSimpleLoginButton(),FRAGMENT_TAG);
-                break;
-            case INDEX_CUSTOM_LOGIN:
-                transaction.replace(android.R.id.content, new FragmentCustomLoginButton(),FRAGMENT_TAG);
-                break;
-        }
-        transaction.commit();
-    }
 }
